01101f12937cbaffec9eca0afcc5dedc9671c656
Andy Lun <andylun1206@gmail.com>
2016-11-15 18:11:08 -0600

    Fixed addTab alert comments, amount restrictions, and redirected page.

--- resources/js/addTab.js
+++ resources/js/addTab.js
@@ -3,21 +3,33 @@
 function getUserInput() {
     var other_person;
 
-    if ( ( document.getElementById( "other_user" ).value ) != null && ( document.getElementById( "other_user" ).value ) != "" ) { other_person = document.getElementById( "other_user" ).value;
+    if ( ( document.getElementById( "other_user" ).value ) != null && ( document.getElementById( "other_user" ).value ) != "" ) {
+        other_person = document.getElementById( "other_user" ).value;
     } else if ( ( document.getElementById( "other_name" ).value ) != null && ( document.getElementById( "other_name" ).value ) != "" ) {
         other_person = document.getElementById( "other_name" ).value
+    } else {
+        alert( "ERROR: Invalid input for names." +
+            "\n\t     Missing username or name/email!" +
+            "\n\t     (If they have an account, please enter their" +
+            "\n\t     username, otherwise please enter their name" +
+            "\n\t     and email address.)");
     }
 
-    if ( document.getElementById( "first_selection" ).value === document.getElementById( "second_selection" ).value ) {
-        alert( "Borrower = Loaner" );
-    } else {
-        addBubble( other_person, document.getElementById( "amount_owed" ).value );
+    if ( other_person != null ) {
+        if ( document.getElementById( "first_selection" ).value === document.getElementById( "second_selection" ).value ) {
+            alert( "ERROR: Invalid input for relationship." +
+                "\n\t     Borrower cannot be the same person as loaner!" );
+        } else {
+            addBubble( other_person, document.getElementById( "amount_owed" ).value );
+        }
     }
 }
 
 function addBubble( otherUserName, amountOwed ) {
+    amountOwed = amountOwed.trim().replace( "$", "" ).replace( ",", "" );
+
     if ( !isNaN( amountOwed ) ) {
-        if ( amountOwed != 0 ) {
+        if ( amountOwed > 0 ) {
             var added = false;
             var posn;
             var numBubbles = sessionStorage.getItem( "numBubbles" );
@@ -47,11 +59,15 @@ function addBubble( otherUserName, amountOwed ) {
             temp = sessionStorage.getObject( "sizes" ); temp.insert( posn, calculateSize( amountOwed ) ); sessionStorage.setObject( "sizes", temp );
             temp = sessionStorage.getObject( "images" ); temp.insert( posn, "default" ); sessionStorage.setObject( "images", temp );
             sessionStorage.setObject( "numBubbles", sessionStorage.getObject( "numBubbles" ) + 1 );
+
+            document.getElementById( "next_btn" ).setAttribute( "href", "mainscreen.html" );
         } else {
-            alert( "Amount is zero!" );
+            alert( "ERROR: Invalid format for amount." +
+                "\n\t     Amount must be greater than 0!" );
         }
     } else {
-        alert( "Amount is not a number!" );
+        alert( "ERROR: Invalid format for amount." +
+            "\n\t     No alphabets or special characters please!" );
     }
 }
 
